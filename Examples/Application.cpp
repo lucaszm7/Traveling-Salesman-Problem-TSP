86ae796ec5604eb14b221b68e22126bd97bf1cd2
Lucas Morais <lucas.morais446@hotmail.com>
2022-09-12 13:23:06 -0300

    Lexicographic order made!

--- Examples/Application.cpp
+++ Examples/Application.cpp
@@ -17,9 +17,12 @@ class TSP : public LGE::Scene_t
 public:
 
     std::vector<glm::vec2> cities;
+    std::vector<int> order;
     std::vector<glm::vec2> bestAnswer;
     unsigned int bestDist;
-    unsigned int totalCities = 5;
+    unsigned int totalCities = 10;
+
+    bool founded = false;
 
     TSP()
     {
@@ -27,23 +30,17 @@ public:
         for (int i = 0; i < totalCities; ++i)
         {
             cities.push_back({ LGE::rand(0, ScreenWidth), LGE::rand(0, ScreenHeight) });
+            order.push_back(i);
         }
         bestAnswer = cities;
         bestDist = calcDist();
     }
 
-    ~TSP() {}
+    ~TSP() override = default;
 
     void foundExatlyTSP()
     {
 
-    }
-
-    void Swap(unsigned int a, unsigned int b)
-    {
-        glm::vec2 c = cities[a];
-        cities[a] = cities[b];
-        cities[b] = c;
     }
 
     unsigned int calcDist()
@@ -57,16 +54,58 @@ public:
         return sum;
     }
 
+    template <typename T>
+    void Swap(std::vector<T>& vals, unsigned int a, unsigned int b) const
+    {
+        T c = vals[a];
+        vals[a] = vals[b];
+        vals[b] = c;
+    }
+
+    void Lexico()
+    {
+        // https://www.quora.com/How-would-you-explain-an-algorithm-that-generates-permutations-using-lexicographic-ordering
+
+        // STEP 1
+        int largestI = -1;
+        for (int i = 0; i < order.size() - 1; ++i)
+        {
+            if (order[i] < order[i + 1]) largestI = i;
+        }
+        if (largestI == -1) { founded = true ; return; }
+
+
+        // STEP 2
+        int largestJ = -1;
+        for (int j = 0; j < order.size(); ++j)
+        {
+            if (order[largestI] < order[j]) largestJ = j;
+        }
+
+
+        // STEP 3
+        Swap<int>(order, largestI, largestJ);
+        Swap<glm::vec2>(cities, largestI, largestJ);
+
+
+        // STEP 4: reverse from largestI + 1, to the end
+        std::reverse(order.begin() + largestI + 1, order.end());
+        std::reverse(cities.begin() + largestI + 1, cities.end());
+    }
+
     void OnUpdate(float fElapsedTime) override
     {
         Draw();
-        Swap(LGE::rand(0, cities.size() - 1), LGE::rand(0, cities.size() - 1));
-        unsigned int dist = calcDist();
-        if (dist < bestDist)
+        for (int i = 0; i < 1000; ++i)
         {
-            bestAnswer = cities;
-            bestDist = dist;
-            std::cout << dist << "\n";
+            Lexico();
+            unsigned int dist = calcDist();
+            if (dist < bestDist)
+            {
+                bestAnswer = cities;
+                bestDist = dist;
+                std::cout << dist << "\n";
+            }
         }
     }
 
@@ -75,7 +114,8 @@ public:
         for (int i = 0; i < cities.size(); ++i)
         {
             DrawPoint(cities[i].x, cities[i].y);
-            if (i + 1 < cities.size()) DrawLine(cities[i].x, cities[i].y, cities[i + 1].x, cities[i + 1].y, { 0.0f, 1.0f, 1.0f, 1.0f });
+            if (!founded)
+                if (i + 1 < cities.size()) DrawLine(cities[i].x, cities[i].y, cities[i + 1].x, cities[i + 1].y, { 0.0f, 1.0f, 1.0f, 1.0f });
         }
 
         for (int i = 0; i < bestAnswer.size(); ++i)
@@ -86,77 +126,21 @@ public:
 
     void OnImGuiRender() override
     {
+        if(founded)
+            ImGui::Text("FOUNDED!");
         ImGui::Text("Smallest Dist: %d", bestDist);
-    }
-};
-
-class Lexico : public LGE::Scene_t
-{
-public:
-
-    std::vector<int> vals = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-    Lexico()
-    {
-        
-    }
-
-    template <typename T>
-    void Swap(std::vector<T>& vals, unsigned int a, unsigned int b)
-    {
-        T c = vals[a];
-        vals[a] = vals[b];
-        vals[b] = c;
-    }
-
-    void OnUpdate(float fElapsedTime) override
-    {
-        // https://www.quora.com/How-would-you-explain-an-algorithm-that-generates-permutations-using-lexicographic-ordering
-        
-        // STEP 1
-        int largestI = -1;
-        for (int i = 0; i < vals.size() - 1; ++i)
-        {
-            if (vals[i] < vals[i + 1]) largestI = i;
-        }
-        if (largestI == -1)  std::cout << "FOUNDED!!!\n";
-
-
-        // STEP 2
-        int largestJ = -1;
-        for (int j = 0; j < vals.size(); ++j)
-        {
-            if (vals[largestI] < vals[j]) largestJ = j;
-        }
-
-
-        // STEP 3
-        Swap<int>(vals, largestI, largestJ);
-
-
-        // STEP 4: reverse from largestI + 1, to the end
-        std::reverse(vals.begin() + largestI + 1, vals.end());
-
-    }
-
-    void OnImGuiRender() override
-    {
-        
         std::string p{};
-        for (int i = 0; i < vals.size(); i++)
-            p += std::to_string(vals[i]) + ", ";
-        ImGui::Text("Vals: %s", p.c_str());
+        for (int i = 0; i < order.size(); i++)
+            p += std::to_string(order[i]) + ", ";
+        ImGui::Text("Order: %s", p.c_str());
+
     }
 };
 
 int main(int argc, char** argv)
 {
     LGE::Application Demo;
-    // Demo.RegisterScene<SceneStaticQuadTree>("Static Quad Tree");
-    // Demo.RegisterScene<PixelDrawing>("Pixel Drawing");
-    // Demo.RegisterScene<MinutesPhysics>("10 Minutes Physics");
     Demo.RegisterScene<TSP>("TSP");
-    Demo.RegisterScene<Lexico>("Lexico");
     Demo.Run ();
 
     return 0;
