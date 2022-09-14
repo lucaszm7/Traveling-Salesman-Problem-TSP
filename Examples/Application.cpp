8e52626a30268720b53288c460da7337bc3cc88a
Lucas Morais <lucas.morais446@hotmail.com>
2022-09-14 20:03:36 -0300

    Finish aprox !!!!!

--- Examples/Application.cpp
+++ Examples/Application.cpp
@@ -2,12 +2,15 @@
 
 // Standart Stuff
 #include <iostream>
+#include <thread>
 #include <memory>
 #include <vector>
 #include <array>
 #include <list>
 #include <fstream>
 #include <chrono>
+#include <utility>
+#include <functional>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,9 +30,12 @@ public:
 
     std::vector<glm::vec2> cities;
     std::vector<int> order;
-    std::vector<int> bestOrder;
+    std::vector<int> bestExactOrder;
+    std::vector<int> bestAproxOrder;
     std::vector<int> bestDisplayOrder;
-    float bestDist = std::numeric_limits<float>::infinity();
+
+    float bestExactDist = std::numeric_limits<float>::infinity();
+    float bestAproxDist = std::numeric_limits<float>::infinity();
     float bestDisplayDist = std::numeric_limits<float>::infinity();
 
     int nodeCount = 0;
@@ -45,7 +51,8 @@ public:
     int subSteps = 100000;
 
     std::chrono::steady_clock::time_point start;
-    std::chrono::duration<double> duration{0};
+    std::chrono::duration<double> durationExact{0};
+    double durationAprox{0};
 
     TSP()
     {
@@ -61,10 +68,19 @@ public:
         founded = false;
         makeFile = false;
 
+        bestExactDist = std::numeric_limits<float>::infinity();
+        bestAproxDist = std::numeric_limits<float>::infinity();
+        bestDisplayDist = std::numeric_limits<float>::infinity();
+
         genAdjacentMatrix();
         initGraph();
 
         totalPermutations = factorial(nodeCount);
+
+        LGE::Timer t;
+        twiceAroundTheTree();
+        durationAprox = t.now();
+
     }
 
     void genAdjacentMatrix()
@@ -145,9 +161,7 @@ public:
             order.push_back(i);
         }
         bestDisplayOrder = order;
-        bestOrder = order;
-        bestDist = calcDist().x; 
-        bestDisplayDist = calcDist().y;
+        bestExactOrder = order;
     }
 
     unsigned long long factorial(int n) const
@@ -160,8 +174,9 @@ public:
         return fac;
     }
 
-    glm::vec2 calcDist()
+    glm::vec2 calcExactDist()
     {
+        // Exact
         glm::vec2 sum = {0, 0};
 
         for (int i = 0; i < order.size() - 1; ++i)
@@ -180,6 +195,176 @@ public:
         sum.y += sqrt(diff.x * diff.x + diff.y * diff.y);
         
         return sum;
+    }
+
+    int calcAproxDist()
+    {
+        // Aprox
+        int aproxWeight = 0;
+        for (int i = 0; i < bestAproxOrder.size() - 1; ++i)
+        {
+            aproxWeight += (float)adjacentMatrix[bestAproxOrder[i]][bestAproxOrder[i + 1]];
+        }
+        aproxWeight += (float)adjacentMatrix[bestAproxOrder[0]][bestAproxOrder[bestAproxOrder.size() - 1]];
+
+        
+        return aproxWeight;
+    }
+
+    void twiceAroundTheTree()
+    {
+        // STEP 1: Complete Graph - Adjacent Matrix
+
+        struct DisjointSets
+        {
+            int* parent, * rnk;
+            int n;
+
+            DisjointSets(int n)
+                : n(n)
+            {
+                parent = new int[n + 1];
+                rnk = new int[n + 1];
+
+                for (int i = 0; i <= n; i++)
+                {
+                    rnk[i] = 0;
+                    parent[i] = i;
+                }
+            }
+
+            int find(int u)
+            {
+                if (u != parent[u])
+                    parent[u] = find(parent[u]);
+                return parent[u];
+            }
+
+            void merge(int x, int y)
+            {
+                x = find(x); y = find(y);
+
+                if (rnk[x] > rnk[y])
+                    parent[y] = x;
+                else // If rnk[x] <= rnk[y]
+                    parent[x] = y;
+
+                if (rnk[x] == rnk[y])
+                    rnk[y]++;
+            }
+        };
+
+
+        // STEP 2: Find Minimum Spanning Tree (kruskal)
+        std::vector<std::pair<int, glm::vec2>> weights;
+        std::vector<glm::vec2> mst;
+        std::vector<bool> addedIndex;
+        weights.reserve(nodeCount * nodeCount);
+        addedIndex.reserve(nodeCount);
+
+
+        for (int i = 0; i < nodeCount; ++i)
+        {
+            for (int j = 0; j < nodeCount; ++j)
+            {
+                if (adjacentMatrix[i][j] == 0) continue;
+                std::pair<int, glm::vec2> map;
+                map.first = adjacentMatrix[i][j];
+                map.second = glm::vec2(i, j);
+                weights.push_back( map );
+            }
+        }
+        
+        std::sort(weights.begin(), weights.end(), [](const std::pair<int, glm::vec2>& a, const std::pair<int, glm::vec2>& b) {
+            return a.first < b.first; });
+
+        /*std::cout << "Weight of Edge - Edge:\n";
+        for (const auto& a : weights)
+        {
+            std::cout << a.first << " - " << a.second.x << ", " << a.second.y << "\n";
+        }*/
+
+        DisjointSets ds(nodeCount);
+
+        std::vector<std::pair<int, glm::vec2> >::iterator it;
+        for (it = weights.begin(); it != weights.end(); it++)
+        {
+            int u = it->second.x;
+            int v = it->second.y;
+
+            int set_u = ds.find(u);
+            int set_v = ds.find(v);
+
+            // Check if the selected edge is creating
+            // a cycle or not (Cycle is created if u
+            // and v belong to same set)
+            if (set_u != set_v)
+            {
+                // Current edge will be in the MST
+                mst.push_back({ u, v });
+
+                // Merge two sets
+                ds.merge(set_u, set_v);
+            }
+        }
+
+        /*std::cout << "Minimal Spanning Tree:\n";
+        for (const auto& a : mst)
+        {
+            std::cout << a.x << ", " << a.y << "\n";
+        }*/
+
+        // STEP 3: Deep First Search
+        // STEP 4: Delete duplicated Vertices
+
+        std::unordered_map<int, std::list<int>> edges;
+        for (auto& edge : mst)
+        {
+            edges[edge.x].push_back(edge.y);
+            edges[edge.y].push_back(edge.x);
+        }
+
+        std::vector<int> dfs;
+        dfs.reserve(nodeCount);
+        std::vector<bool> visited;
+        visited.resize(nodeCount);
+
+        std::function<void(int v)> make_dfs;
+        make_dfs = [&](int v)
+        {
+            visited[v] = true;
+            dfs.push_back(v);
+
+            for (auto i = edges[v].begin(); i != edges[v].end(); ++i)
+            {
+                if (!visited[(*i)])
+                    make_dfs(*i);
+            }
+            return;
+        };
+
+        make_dfs(ds.find(0));
+        bestAproxOrder = dfs;
+        bestAproxDist = calcAproxDist();
+
+        for (int i = 0; i < nodeCount; ++i)
+        {
+            make_dfs(ds.find(i));
+            auto dist = calcAproxDist();
+            if (dist < bestAproxDist)
+            {
+                bestAproxDist = dist;
+                bestAproxOrder = dfs;
+            }
+        }
+        /*std::cout << "Deepth First Search: \n";
+        for (auto a : dfs)
+        {
+            std::cout << a << " - ";
+        }
+        std::cout << "\n";*/
+
+        
     }
 
     template <typename T>
@@ -224,17 +409,28 @@ public:
         makeFile = true;
         auto fileOutputName = fileName.substr(0, fileName.find_first_of('.'));
         double percentage = (((double)countPermutations * 100.0f) / (double)totalPermutations);
-        fileOutputName += "_exact_results.txt";
+        fileOutputName += "_results.txt";
         std::cout << "\nOutput file: " << fileOutputName << "\n";
 
         std::ofstream results(fileOutputName);
-        results << "Time Taken: " << std::to_string(duration.count()) << "s\n";
+        results << "-------------------------------------------------------------------\n";
+        results << "------------------------- Exact Algorithm -------------------------\n";
+        results << "Exact Time Taken: " << std::to_string(durationExact.count()) << "s\n";
         results << "Percentage Complete: " << percentage << "%\n";
-        std::string o{};
-        for (int i = 0; i < order.size(); i++)
-            o += std::to_string(bestOrder[i]) + ", ";
-        results << "Best Order: " << o << "\n";
-        results << "Minimal Distance: " << bestDist << "\n";
+        std::string o{}, a{};
+        for (int i = 0; i < bestExactOrder.size(); i++)
+            o += std::to_string(bestExactOrder[i]) + ", ";
+        for (int i = 0; i < bestAproxOrder.size(); i++)
+            a += std::to_string(bestAproxOrder[i]) + ", ";
+        results << "Best Exact Order: " << o << "\n";
+        results << "Minimal Exact Distance: " << bestExactDist << "\n";
+        results << "-------------------------------------------------------------------\n";
+        results << "------------------------- 2-Aprox Algorithm -----------------------\n";
+        results << "Aprox Time Taken: " << durationAprox << "\n";
+        results << "Best Aprox Order: " << a << "\n";
+        results << "Minimal Aprox Distance: " << bestAproxDist << "\n";
+        results << "--------------------------------------------------------------------\n";
+
         results.close();
     }
 
@@ -251,12 +447,12 @@ public:
             }
 
             Lexico();
-            glm::vec2 dist = calcDist();
+            glm::vec2 dist = calcExactDist();
 
-            if (dist.x < bestDist)
+            if (dist.x < bestExactDist)
             {
-                bestOrder = order;
-                bestDist = dist.x;
+                bestExactOrder = order;
+                bestExactDist = dist.x;
             }
             if (dist.y < bestDisplayDist)
             {
@@ -293,14 +489,18 @@ public:
     {
         std::string c{};
         std::string o{};
+        std::string a{};
 
         ImGui::Combo(" ", &selectedFile, fileNames, IM_ARRAYSIZE(fileNames));
         ImGui::SameLine();
         if (ImGui::Button("Reset"))
             reset();
 
-        if(!founded) duration = std::chrono::high_resolution_clock::now() - start;
-        ImGui::Text("Time taked: %.2f s", duration.count());
+        if (ImGui::Button("Gen Result File"))
+            genOutputFile();
+
+        if(!founded) durationExact = std::chrono::high_resolution_clock::now() - start;
+        ImGui::Text("Exact Time taked: %.2f s", durationExact.count());
 
         double percentage = (((double)countPermutations * 100.0f) / (double)totalPermutations);
         ImGui::Text("Completeness: %.6f%%", percentage);
@@ -310,14 +510,25 @@ public:
             ImGui::Text("FINISHED!");
         }
 
-        ImGui::Text("Smallest Dist: %.2f", bestDist);
-        for (int i = 0; i < order.size(); i++)
-            o += std::to_string(bestOrder[i]) + ", ";
-        ImGui::Text("Best Order: \n%s", o.c_str());
-
         for (const auto& ord : order)
             c += std::to_string(ord) + ", ";
-        ImGui::Text("Order: \n%s", c.c_str());
+        ImGui::Text("Order: \n%s\n", c.c_str());
+
+        ImGui::Separator();
+
+        ImGui::Text("Smallest Exact Dist: %.2f", bestExactDist);
+        for (int i = 0; i < order.size(); i++)
+            o += std::to_string(bestExactOrder[i]) + ", ";
+        ImGui::Text("Best Exact Order: \n%s\n", o.c_str());
+        
+        ImGui::Separator();
+
+        ImGui::Text("Aprox Time Take: %.2f", durationAprox);
+        ImGui::Text("Smallest Aprox Dist: %.2f", bestAproxDist);
+        for (int i = 0; i < bestAproxOrder.size(); i++)
+            a += std::to_string(bestAproxOrder[i]) + ", ";
+        ImGui::Text("Best Aprox Order: \n%s", a.c_str());
+        
     }
 };
 
